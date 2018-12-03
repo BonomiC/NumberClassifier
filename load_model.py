7d83feee5a2bd2dc341d51ea2f442b42baf627f9
Christopher Bonomi <bonomi.c11@gmail.com>
2018-12-03 03:59:45 -0500

    added gui for drawing numbers. current not very accurate. modified mnist data in attempt to compensate

--- load_model.py
+++ load_model.py
@@ -1,8 +1,11 @@
 import numpy as np
 import tensorflow as tf
-mnist = tf.keras.datasets.mnist
-
 from create_model import create_model
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')
+
+mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
@@ -16,21 +19,37 @@ print("Restored model, accuracy: {:5.2f}%".format(100*acc))
 model.summary()
 
 for i in range(10):
+    rnd = np.random.randint(0, len(x_test))
+    expected = y_test[rnd]
+    test = x_test[rnd].reshape(1, 784)
+    prediction = model.predict(test)
+    predicted = np.argmax(prediction)
+    print "Expected: ", expected
+    print "Predicted: ", predicted
+    print "****************"
+    if expected == predicted:
+        print "CORRECT"
+    else:
+        print "INCORRECT"
+    print "****************"
+
+fig = plt.figure()
+for i in range(9):
 	rnd = np.random.randint(0, len(x_test))
 	expected = y_test[rnd]
 	test = x_test[rnd].reshape(1, 784)
+
 	prediction = model.predict(test)
 	predicted = np.argmax(prediction)
-	print "Expected: ", expected
-	print "Predicted: ", predicted
-	print "****************"
-	if expected == predicted:
-		print "CORRECT"
-	else:
-		print "INCORRECT"
-	print "****************"
+	plt.subplot(3, 3, i+1)
+	plt.tight_layout()
+	plt.imshow(x_test[rnd], cmap='gray', interpolation='none')
+	plt.title("Digit: {}, Prediction: {}".format(y_test[rnd], predicted))
+	plt.xticks([])
+	plt.yticks([])
+plt.show()
 
 # print(type(test))
 
 # for i, p in enumerate(prediction[0]):
-	# print i, p
+# print i, p
